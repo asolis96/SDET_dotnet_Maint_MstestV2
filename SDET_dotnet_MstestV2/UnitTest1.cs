2a95ce2b997dbda2157adc67e1b988290b1c45b6
Luis Osuna <luis.osuna@unosquare.com>
2020-05-08 22:41:43 -0500

    Add project files.

---
+++ SDET_dotnet_MstestV2/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SDET_dotnet_MstestV2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
