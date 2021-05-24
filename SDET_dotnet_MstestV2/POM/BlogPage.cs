5e7acca0822c264200a15f10542f5b7fb3df6c63
Adrian Solis <adrian.solis@unosquare.com>
2021-05-24 18:52:33 -0500

    Added BlogPage

---
+++ SDET_dotnet_MstestV2/POM/BlogPage.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SDET_dotnet_MstestV2.Base;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDET_dotnet_MstestV2.POM
+{
+    class BlogPage : UnoPages
+    {
+        [FindsBy(How = How.XPath, Using = "//*[@class='breadcrumb-container']//h1")]
+        private IWebElement PageTitle { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='search-bar']")]
+        private IWebElement SearchBar { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//button[@type='submit']")]
+        private IWebElement SubmitButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@class='results-title']")]
+        private IWebElement ResultsTitle { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@class='title-header']//a")]
+        private IList<IWebElement> SearchResults { get; set; }
+
+
+        private string SearchText;
+        public BlogPage() : base()
+        {
+            PageFactory.InitElements(driver, this);
+            SearchText = "";
+
+        }
+
+        public BlogPage ValidatePageTitleIsPresent()
+        {
+            Assert.IsTrue(PageTitle.Displayed);
+            return this;
+        }
+
+        public BlogPage SearchForInPage(string text)
+        {
+            SearchText = text;
+            browser.SendText(SearchBar, text);
+            browser.Click(SubmitButton);
+            return this;
+        }
+
+        public BlogPage ValidateSearchResults()
+        {
+            Assert.IsTrue(ResultsTitle.Text.Contains(SearchText), "Result title does not contains the search text");
+            Assert.IsTrue(SearchResults.Count() > 0, "The list is empty, search result was empty");
+            //Assert.IsTrue(SearchResults[0].Text.Contains(SearchText));
+            
+            return this;
+        }
+    }
+}
--- SDET_dotnet_MstestV2/TestCases.cs
+++ SDET_dotnet_MstestV2/TestCases.cs
@@ -7,21 +7,28 @@ namespace SDET_dotnet_MstestV2
     public class TestCases
     {
         HomePage homepage;
+        BlogPage blogPage;
         [TestInitialize]
         public void BeforeTest()
         {
             homepage = new HomePage();
+            blogPage = new BlogPage();
         }
         [TestMethod]
         public void Unosquare_BlogValidation()
         {
-            homepage.GoToServicesAndPracticeAreas();
+            //homepage.GoToServicesAndPracticeAreas();
             /*Add a new Page object name Blog
              *Implement all neded to use Blog in the test class (Constructor, WebElements, etc)
              *Perform the following flow, Go to www.unosquare.com
              * Go to Blog - Vlidate using Asserts, this element is present DIGITAL TRANSFORMATION BLOG
              * Search for Quality Assurance, validate with Asserts that at least one result is displayed
              */
+            homepage.GoToBlog();
+            blogPage.ValidatePageTitleIsPresent();
+            blogPage.SearchForInPage("Quality Assurance");
+            blogPage.ValidateSearchResults();
+            
         }
 
     }
