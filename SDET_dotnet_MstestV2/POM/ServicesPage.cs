c0668bbec6b96d32acfa29a85e4818cb78e3ee13
Adrian Solis <adrian.solis@unosquare.com>
2021-05-25 12:39:58 -0500

    added ServicesPage Class

---
+++ SDET_dotnet_MstestV2/POM/ServicesPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SDET_dotnet_MstestV2.Base;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDET_dotnet_MstestV2.POM
+{
+
+    class ServicesPage : UnoPages
+    {
+        [FindsBy( How = How.XPath, Using = "//*[@class='subtitle' and contains(text(),'Trusted')]")]
+        private IWebElement TrustedByIndustriesText { get; set; }
+
+        public ServicesPage() : base()
+        {
+
+        }
+
+
+    }
+}
